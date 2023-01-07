6865edb83c05d403ae3f16ba2e8cf35b38660b65
GSNandkhedkar <gaurinandkhedkar99@gmail.com>
2023-01-07 20:04:21 +0530

    initial commit

---
+++ solutions/day-03/day-3/src/index.js
@@ -0,0 +1,89 @@
+// index.js
+import React from 'react'
+import ReactDOM from 'react-dom'
+// JSX element, header
+
+const welcome = "Welcome to 30 Days Of React";
+const title = "Getting Started React";
+const subtitle = "JavaScript Library";
+const author = {
+  firstName: 'Gauri',
+  lastName: 'Nandkhedkar',
+};
+const date = 'January 07, 2023';
+
+const header = (
+  <header>
+    <div className='header-wrapper'>
+      <h1>{welcome}</h1>
+      <h2>{title}</h2>
+      <h3>{subtitle}</h3>
+      <p>Instructor: {author.firstName} {author.lastName}</p>
+      <small>Date: {date}</small>
+    </div>
+  </header>
+)
+
+const numOne = 3;
+const numTwo = 2;
+
+const result = (
+  <p>
+    {numOne} + {numTwo} = {numOne + numTwo}
+  </p>
+)
+
+const yearBorn = 1999;
+const currentYear = new Date().getFullYear();
+const age = currentYear - yearBorn;
+const personAge = (
+  <p>
+    {' '}
+    {author.firstName} {author.lastName} is {age} years old.
+  </p>
+);
+
+const techs = ['HTML', 'CSS', 'Javascript'];
+const techsFormatted = techs.map((tech) => <li>{tech}</li>);
+
+// JSX element, main
+const main = (
+  <main>
+    <div className='main-wrapper'>
+      <p>
+        Prerequisite to get started{' '}
+        <strong>
+          <em>react.js</em>
+        </strong>
+        :
+      </p>
+      <ul>{techsFormatted}</ul>
+      {result}
+      {personAge}
+    </div>
+  </main>
+);
+
+const copyRight = "Copyright 2020";
+
+// JSX element, footer
+const footer = (
+  <footer>
+    <div className='footer-wrapper'>
+      <p>{copyRight}</p>
+    </div>
+  </footer>
+)
+
+// JSX element, app
+const app = (
+  <div className='app'>
+    {header}
+    {main}
+    {footer}
+  </div>
+)
+
+const rootElement = document.getElementById('root')
+// we render the JSX element using the ReactDOM package
+ReactDOM.render(app, rootElement)
